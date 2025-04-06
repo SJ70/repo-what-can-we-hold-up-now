38097b2102f7c187616e9d12379c291b513cce6c
SJ70 <seojae70@gmail.com>
2025-04-06 18:08:13 +0900

    chore: 몬스터 배열 기본적으로 무게 순으로 정렬

--- src/const/monsters.js
+++ src/const/monsters.js
@@ -1,118 +1,120 @@
-const MONSTERS = Object.freeze([
-  {
-    name : 'Animal', 
-    kor_name : '짐승', 
-    img : '/img/monster/animal.png', 
-    weight: 4
-  },
-  {
-    name : 'Banger', 
-    kor_name : '폭탄 해골', 
-    img : '/img/monster/banger.png', 
-    weight: 0
-  },
-  {
-    name : 'Bowtie', 
-    kor_name : '나비넥타이', 
-    img : '/img/monster/bowtie.png', 
-    weight: 7
-  },
-  {
-    name : 'Clown', 
-    kor_name : '광대', 
-    img : '/img/monster/clown.png', 
-    weight: 13
-  },
-  {
-    name : 'Peeper', 
-    kor_name : '주시자', 
-    img : '/img/monster/peeper.png', 
-    weight: Infinity
-  },
-  {
-    name : 'Apex Predator', 
-    kor_name : '최상위 포식자', 
-    img : '/img/monster/apex-predator.png', 
-    weight: Infinity
-  },
-  {
-    name : 'Mentalist', 
-    kor_name : '멘탈리스트', 
-    img : '/img/monster/mentalist.png', 
-    weight: 4
-  },
-  {
-    name : 'Chef', 
-    kor_name : '요리사', 
-    img : '/img/monster/chef.png', 
-    weight: 9
-  },
-  {
-    name : 'Gnome', 
-    kor_name : '노움', 
-    img : '/img/monster/gnome.png', 
-    weight: 0
-  },
-  {
-    name : 'Reaper', 
-    kor_name : '리퍼', 
-    img : '/img/monster/reaper.png', 
-    weight: 9
-  },
-  {
-    name : 'Headsman', 
-    kor_name : '헤드맨', 
-    img : '/img/monster/headsman.png', 
-    weight: 13
-  },
-  {
-    name : 'Hidden', 
-    kor_name : '히든', 
-    img : '/img/monster/hidden.png', 
-    weight: 4
-  },
-  {
-    name : 'Huntsman', 
-    kor_name : '사냥꾼', 
-    img : '/img/monster/huntsman.png', 
-    weight: 9
-  },
-  {
-    name : 'Robe', 
-    kor_name : '로브', 
-    img : '/img/monster/robe.png', 
-    weight: 13
-  },
-  {
-    name : 'Spewer', 
-    kor_name : '스퓨어', 
-    img : '/img/monster/spewer.png', 
-    weight: 4
-  },
-  {
-    name : 'Trudge', 
-    kor_name : '트러지', 
-    img : '/img/monster/trudge.png', 
-    weight: 13
-  },
-  {
-    name : 'Shadow Child', 
-    kor_name : '어둠의 아이', 
-    img : '/img/monster/shadow-child.png', 
-    weight: 9
-  },
-  {
-    name : 'Upscream', 
-    kor_name : '업스크림', 
-    img : '/img/monster/upscream.png', 
-    weight: 4
-  },
-  {
-    name : 'Rugrat', 
-    kor_name : '러그랫', 
-    img : '/img/monster/rugrat.png', 
-    weight: 4
-  },
-]);
+const MONSTERS = Object.freeze(
+  [
+    {
+      name : 'Animal', 
+      kor_name : '짐승', 
+      img : '/img/monster/animal.png', 
+      weight: 4
+    },
+    {
+      name : 'Banger', 
+      kor_name : '폭탄 해골', 
+      img : '/img/monster/banger.png', 
+      weight: 0
+    },
+    {
+      name : 'Bowtie', 
+      kor_name : '나비넥타이', 
+      img : '/img/monster/bowtie.png', 
+      weight: 7
+    },
+    {
+      name : 'Clown', 
+      kor_name : '광대', 
+      img : '/img/monster/clown.png', 
+      weight: 13
+    },
+    {
+      name : 'Peeper', 
+      kor_name : '주시자', 
+      img : '/img/monster/peeper.png', 
+      weight: Infinity
+    },
+    {
+      name : 'Apex Predator', 
+      kor_name : '최상위 포식자', 
+      img : '/img/monster/apex-predator.png', 
+      weight: Infinity
+    },
+    {
+      name : 'Mentalist', 
+      kor_name : '멘탈리스트', 
+      img : '/img/monster/mentalist.png', 
+      weight: 4
+    },
+    {
+      name : 'Chef', 
+      kor_name : '요리사', 
+      img : '/img/monster/chef.png', 
+      weight: 9
+    },
+    {
+      name : 'Gnome', 
+      kor_name : '노움', 
+      img : '/img/monster/gnome.png', 
+      weight: 0
+    },
+    {
+      name : 'Reaper', 
+      kor_name : '리퍼', 
+      img : '/img/monster/reaper.png', 
+      weight: 9
+    },
+    {
+      name : 'Headsman', 
+      kor_name : '헤드맨', 
+      img : '/img/monster/headsman.png', 
+      weight: 13
+    },
+    {
+      name : 'Hidden', 
+      kor_name : '히든', 
+      img : '/img/monster/hidden.png', 
+      weight: 4
+    },
+    {
+      name : 'Huntsman', 
+      kor_name : '사냥꾼', 
+      img : '/img/monster/huntsman.png', 
+      weight: 9
+    },
+    {
+      name : 'Robe', 
+      kor_name : '로브', 
+      img : '/img/monster/robe.png', 
+      weight: 13
+    },
+    {
+      name : 'Spewer', 
+      kor_name : '스퓨어', 
+      img : '/img/monster/spewer.png', 
+      weight: 4
+    },
+    {
+      name : 'Trudge', 
+      kor_name : '트러지', 
+      img : '/img/monster/trudge.png', 
+      weight: 13
+    },
+    {
+      name : 'Shadow Child', 
+      kor_name : '어둠의 아이', 
+      img : '/img/monster/shadow-child.png', 
+      weight: 9
+    },
+    {
+      name : 'Upscream', 
+      kor_name : '업스크림', 
+      img : '/img/monster/upscream.png', 
+      weight: 4
+    },
+    {
+      name : 'Rugrat', 
+      kor_name : '러그랫', 
+      img : '/img/monster/rugrat.png', 
+      weight: 4
+    },
+  ].sort((a, b) => a.weight - b.weight)
+);
 
-
+export default MONSTERS;
